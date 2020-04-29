1f04821b9b1c068fb1267a4e4f8d9a4567935edf
persianpros <persianpros@yahoo.com>
2020-04-29 10:30:04 +0430

    py files: E101 and E121 PEP8 cleanup
    Indentation contains mixed spaces and tabs (E101)
    Continuation line under-indented for hanging indent (E121)

--- recipes-bsp/linux/linux-uclan-4.4.35/findkerneldevice.py
+++ recipes-bsp/linux/linux-uclan-4.4.35/findkerneldevice.py
@@ -5,12 +5,12 @@ sys.path.append("/usr/lib/enigma2/python")
 from boxbranding import getMachineMtdKernel
 
 try:
-	STARTUP = open('/boot/STARTUP', 'r').readline().split(' ')
-	STARTUP = STARTUP[1].split('.')
-	kerneldevice = STARTUP[1]
-	if kerneldevice == "kernel":
-		kerneldevice = getMachineMtdKernel()
-	#print kerneldevice
-	os.symlink("/dev/" + kerneldevice, '/dev/kernel')
+    STARTUP = open('/boot/STARTUP', 'r').readline().split(' ')
+    STARTUP = STARTUP[1].split('.')
+    kerneldevice = STARTUP[1]
+    if kerneldevice == "kernel":
+        kerneldevice = getMachineMtdKernel()
+    #print kerneldevice
+    os.symlink("/dev/" + kerneldevice, '/dev/kernel')
 except:
-	pass
+    pass
